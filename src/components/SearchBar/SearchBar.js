98a8b919db4c55a483960cd1a23f74378be91b38
Erik Montes <zomberik@gmail.com>
2018-02-02 00:48:53 -0800

    created filter button subcomponents

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './SearchBar.css';
 
+import FilterButton from '../FilterButton/FilterButton';
+
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
@@ -42,22 +44,28 @@ class SearchBar extends React.Component {
     this.setState({location: event.target.value});
   }
 
-  handleSearch(event) {
+  handleSearch() {
     this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+  }
 
-    event.preventDefault();
+  handleSearchAndSort(sortByOptionValue) {
+    this.handleSearch();
+    this.handleSortByChange(sortByOptionValue);
   }
 
   renderSortByOptions() {
+
     return Object.keys(this.sortByOptions).map(sortByOption => {
+      
       let sortByOptionValue = this.sortByOptions[sortByOption];
+      let filterClass = this.getSortByClass(sortByOptionValue);
+
       return (
-          <li 
-          className =      {this.getSortByClass(sortByOptionValue)}
-          key={sortByOptionValue}
-          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
-                {sortByOption}
-             </li>
+          <FilterButton labelValue={sortByOption}
+                        key={sortByOptionValue}
+                        handleSearchAndSort={this.handleSearchAndSort.bind(this, sortByOptionValue)}
+                        getSortByClass={filterClass}
+                        />
       );
     });
   }
